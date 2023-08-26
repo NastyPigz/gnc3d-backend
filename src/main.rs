022cfa470650fd39e75ffbb96cea0b7ca382b240
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2023-08-26 12:54:57 -0400

    bruh prod

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,16 @@ mod server;
 mod session;
 mod constants;
 
+
+
 #[tokio::main]
 async fn main() -> Result<(), hyper::Error> {
     // DO NOT move this is where app states are supposed to be declared
     // let state = server::PeerMap::new(Mutex::new(HashMap::new()));
     let rooms = server::RoomMap::new(Mutex::new(HashMap::new()));
 
-    let addr = "127.0.0.1:8080".to_string().parse().unwrap();
-    // let addr = "0.0.0.0:443".to_string().parse().unwrap();
+    // let addr = "127.0.0.1:8080".to_string().parse().unwrap();
+    let addr = "0.0.0.0:443".to_string().parse().unwrap();
 
     let make_svc = make_service_fn(move |conn: &AddrStream| {
         let remote_addr = conn.remote_addr();
